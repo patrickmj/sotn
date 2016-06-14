b06a9bc7d58a62c137c23ecfc9a9025da4626bce
patrickmj <patrickmjchnm@gmail.com>
2016-06-14 18:46:00 -0400

    minimize search form
    kill element search.
    move id search to bottom.

--- items/search-form.php
+++ items/search-form.php
@@ -66,99 +66,6 @@ $statesArray = array(
 ?>
 
 <form <?php echo tag_attributes($formAttributes); ?>>
-    <div id="search-keywords" class="field">
-        <h3><?php echo $this->formLabel('keyword-search', __('Search for Keywords')); ?></h3>
-        <div class="inputs">
-        <?php
-            echo $this->formText(
-                'search',
-                @$_REQUEST['search'],
-                array('id' => 'keyword-search', 'size' => '40')
-            );
-        ?>
-        </div>
-    </div>
-    <div id="search-narrow-by-fields" class="field">
-        <h3><div class="label"><?php echo __('Narrow by Specific Fields'); ?></div></h3>
-        <div class="inputs">
-        <?php
-        // If the form has been submitted, retain the number of search
-        // fields used and rebuild the form
-        if (!empty($_GET['advanced'])) {
-            $search = $_GET['advanced'];
-        } else {
-            $search = array(array('field'=>'','type'=>'','value'=>''));
-        }
-
-        //Here is where we actually build the search form
-        foreach ($search as $i => $rows): ?>
-            <div class="search-entry">
-                <?php
-                //The POST looks like =>
-                // advanced[0] =>
-                //[field] = 'description'
-                //[type] = 'contains'
-                //[terms] = 'foobar'
-                //etc
-                echo $this->formSelect(
-                    "advanced[$i][element_id]",
-                    @$rows['element_id'],
-                    array(
-                        'title' => __("Search Field"),
-                        'id' => null,
-                        'class' => 'advanced-search-element'
-                    ),
-                    get_table_options('Element', null, array(
-                        'record_types' => array('Item', 'All'),
-                        'sort' => 'orderBySet')
-                    )
-                );
-                echo $this->formSelect(
-                    "advanced[$i][type]",
-                    @$rows['type'],
-                    array(
-                        'title' => __("Search Type"),
-                        'id' => null,
-                        'class' => 'advanced-search-type'
-                    ),
-                    label_table_options(array(
-                        'contains' => __('contains'),
-                        'does not contain' => __('does not contain'),
-                        'is exactly' => __('is exactly'),
-                        'is empty' => __('is empty'),
-                        'is not empty' => __('is not empty'),
-                        'starts with' => __('starts with'),
-                        'ends with' => __('ends with'))
-                    )
-                );
-                echo $this->formText(
-                    "advanced[$i][terms]",
-                    @$rows['terms'],
-                    array(
-                        'size' => '20',
-                        'title' => __("Search Terms"),
-                        'id' => null,
-                        'class' => 'advanced-search-terms'
-                    )
-                );
-                ?>
-                <button type="button" class="remove_search" disabled="disabled" style="display: none;"><?php echo __('Remove field'); ?></button>
-            </div>
-        <?php endforeach; ?>
-        </div>
-        <button type="button" class="add_search"><?php echo __('Add a Field'); ?></button>
-    </div>
-
-    <div id="search-by-range" class="field">
-        <h3><?php echo $this->formLabel('range', __('Search by a range of Item (i.e, Newspaper Front Page) ID#s (example: 1-4, 156, 79)')); ?></h3>
-        <div class="inputs">
-        <?php
-            echo $this->formText('range', @$_GET['range'],
-                array('size' => '40')
-            );
-        ?>
-        </div>
-    </div>
 
     <div class="field">
         <h3><?php echo $this->formLabel('collection-search', __('Search By Newspaper')); ?></h3>
@@ -335,7 +242,17 @@ $statesArray = array(
     </div>
     </div>
     
-    
+
+    <div id="search-by-range" class="field">
+        <h3><?php echo $this->formLabel('range', __('Search by a range of Item (i.e, Newspaper Front Page) ID#s (example: 1-4, 156, 79)')); ?></h3>
+        <div class="inputs">
+        <?php
+            echo $this->formText('range', @$_GET['range'],
+                array('size' => '40')
+            );
+        ?>
+        </div>
+    </div>
     
     
     
